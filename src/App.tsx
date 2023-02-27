f05f93a7b4ddba87bb9e7586c782203f03598d94
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-02-27 22:19:25 +0900

    [Style] App component styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
+import Suggest from "./components/Suggest";
+import MainPage from "./components/MainPage";
+import "./App.css";
+
 function App() {
   return (
-    <h1>바슐랭</h1>
+    <div className="app">
+      <div className="app__header">
+        <div className="app__header__logo-container">
+          <img
+            className="app__header__logo-img"
+            src="../../logo.png"
+            alt="vanilla-coding-logo"
+          />
+        </div>
+      </div>
+      <div className="app__content">
+        <MainPage />
+      </div>
+    </div>
   );
 }
 
